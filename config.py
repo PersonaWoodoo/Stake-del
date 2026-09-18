9f5775577b4bb39733f74e51ced8bf97c0c01ea1
PersonaWoodoo <shortsik44@gmail.com>
2026-09-18 11:05:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+ORDERS_CHANNEL_ID = int(os.getenv("ORDERS_CHANNEL_ID"))
+STARS_PER_ATTEMPT = int(os.getenv("STARS_PER_ATTEMPT", "10"))
+REFS_PER_ATTEMPT = int(os.getenv("REFS_PER_ATTEMPT", "5"))
+START_ATTEMPTS = int(os.getenv("START_ATTEMPTS", "1"))
